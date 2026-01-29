bc49335d9535be691a127d278eb002a2224f3b35
him6221-oss <him6221@aim.com>
2026-01-28 19:26:52 -0600

    Add files via upload

---
+++ hero.py
@@ -0,0 +1,32 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+Please ensure your code meets the following before submitting:
+
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable (e.g., Character Name).
+[ ] 3. Define an Integer variable (e.g., Level or Age).
+[ ] 4. Define a Float variable (e.g., Speed or Power).
+[ ] 5. Define a Boolean variable (e.g., is_alive).
+[ ] 6. Print all variables to the screen using F-Strings.
+[ ] 7. Upload to GitHub and add instructor as a contributor.
+-----------------------------------------------------------------------
+"""
+
+
+# Define character variables
+
+character_name = "Veagon"
+level = 20
+Strength = 15
+is_alive = True
+
+# Display character information using f-strings
+
+print("\n--- CHARACTER PROFILE ---\n")
+
+print(f"Name: {character_name}")
+print(f"Level: {level}")
+print(f"Strength: {Strength}")
+print(f"Alive: {is_alive}")
