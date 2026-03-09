3e07149e8f5a44d75be9aef13d1d0a1170144571
him6221-oss <him6221@aim.com>
2026-03-08 21:13:54 -0500

    Add files via upload

---
+++ data_mapper.py
@@ -0,0 +1,54 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 8A: OPTION A - NATO TRANSLATOR
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. NATO_ALPHABET constant is a dictionary (Full A-Z).
+[ ] 3. Program takes a word and uppercases it.
+[ ] 4. Program loops through letters and prints NATO words.
+[ ] 5. A 'try/except' block handles punctuation or numbers.
+-----------------------------------------------------------------------
+"""
+NATO_ALPHABET = {
+    "A": "Alpha", "B": "Bravo", "C": "Charlie", # ...COMPLETE A-Z
+}
+
+word = input("Enter word to spell: ").upper()
+
+# TODO: Loop through each character
+# TODO: try to print the NATO code, except if character is missing
+
+NATO_ALPHABET = { 
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliett",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "Xray",
+    "Y": "Yankee",
+    "Z": "Zulu"
+}
+
+for letter in word:
+    try:
+        print(NATO_ALPHABET[letter])
+    except KeyError:
+        print("Character not in alphabet")
